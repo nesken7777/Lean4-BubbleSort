8db65418f6625c3a5fc8b557bff6c7524186262e
nesken7777 <51521915+nesken7777@users.noreply.github.com>
2024-12-04 14:46:49 +0900

    完成

--- BubbleSort/Basic.lean
+++ BubbleSort/Basic.lean
@@ -12,8 +12,6 @@ theorem lt_of_lt_sub {a b c : Nat} : a < b - c → a < b := by
       a < b - c := h
       b - c ≤ b := Nat.sub_le b c
 
-#print lt_of_lt_sub
-
 def bubbleSort_for [Inhabited α] [Ord α] (arr : Array α) : Array α := Id.run do
   let mut arr := arr
   for i in [0:arr.size] do
@@ -37,13 +35,52 @@ def bubbleSort [Ord α] (arr : Array α) : Array α :=
         |.lt |.eq => loop₂ arr i (⟨j + 1, succ_j_lt_arr_size⟩)
       else
         arr
+    termination_by arr.size - 1 - i - j
     if h : i < arr.size then
-      loop₁ ((loop₂ arr ⟨i, h⟩ ⟨0, Nat.zero_lt_of_lt h⟩)) (i + 1)
+      loop₁ (loop₂ arr ⟨i, h⟩ ⟨0, Nat.zero_lt_of_lt h⟩) (i + 1)
     else
       arr
   termination_by arr.size - i
   decreasing_by
-    sorry
+    let rec loop₂_size_eq [Ord α] (arr' arr : Array α) {i j : Nat} {hi : i < arr'.size} {hj : j < arr'.size} (h_size : arr'.size = arr.size) : (bubbleSort.loop₁.loop₂ arr' ⟨i, hi⟩ ⟨j, hj⟩).size = arr.size := by
+      generalize h : arr'.size - 1 - i - j = k
+      induction k generalizing arr' i j
+      case zero =>
+        unfold bubbleSort.loop₁.loop₂
+        split
+        case isTrue h₁ =>
+          have h₃ : arr'.size - 1 - i - j ≠ 0 := by exact Nat.sub_ne_zero_iff_lt.mpr h₁
+          contradiction
+        case isFalse =>
+          exact h_size
+      case succ n ih =>
+        unfold bubbleSort.loop₁.loop₂
+        split
+        case isTrue h₁ =>
+          have h_eq_n : arr'.size - 1 - i - (j + 1) = n := by
+            calc
+              arr'.size - 1 - i - (j + 1) = (arr'.size - 1 - i - j) - 1 := rfl
+              _ = n := Nat.sub_eq_of_eq_add h
+          split
+          case h_1 hgt =>
+            have h_size_swap : (arr'.swap ⟨j, hj⟩ ⟨↑(⟨j, hj⟩ : Fin arr'.size) + 1, bubbleSort.loop₁.loop₂.proof_2 arr' ⟨i, hi⟩ ⟨j, hj⟩ h₁⟩).size = arr'.size :=
+              arr'.size_swap ⟨j, hj⟩ ⟨↑(⟨j, hj⟩ : Fin arr'.size) + 1, bubbleSort.loop₁.loop₂.proof_2 arr' ⟨i, hi⟩ ⟨j, hj⟩ h₁⟩
+            apply ih
+            exact Eq.trans h_size_swap h_size
+            rw[h_size_swap]
+            exact h_eq_n
+          case h_2 hlt =>
+            apply ih
+            exact h_size
+            exact h_eq_n
+          case h_3 heq =>
+            apply ih
+            exact h_size
+            exact h_eq_n
+        case isFalse =>
+          exact h_size
+    rw[loop₂_size_eq arr arr rfl]
+    exact Nat.sub_succ_lt_self arr.size i h
 
   loop₁ arr 0
 
