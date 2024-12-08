109ca1ee70cbba8b39737b63784aba90ed1e2023
nesken7777 <51521915+nesken7777@users.noreply.github.com>
2024-12-08 18:57:25 +0900

    zenn向けにいろいろ調整

--- BubbleSort/Basic.lean
+++ BubbleSort/Basic.lean
@@ -1,4 +1,31 @@
-def hello := "world"
+def bubbleSort [Ord α] (arr : Array α) : Array α :=
+  let rec loop₁ [Ord α] (arr : Array α) (i : Nat) : Array α :=
+    let rec loop₂ [Ord α] (arr : Array α) (i : Nat) (j : Nat) : Array α :=
+      if h_index : j < arr.size - 1 - i then
+        match Ord.compare arr[j] arr[j + 1] with
+        |.gt => loop₂ (arr.swap j (j + 1)) i (j + 1)
+        |.lt |.eq => loop₂ arr i (j + 1)
+      else
+        arr
+    if i < arr.size then
+      loop₁ (loop₂ arr i 0) (i + 1)
+    else
+      arr
+  termination_by arr.size - i
+  decreasing_by
+    have loop₂_size_eq {arr' arr : Array α} {i j : Nat} (h_size : arr'.size = arr.size) :
+      (bubbleSort.loop₁.loop₂ arr' i j).size = arr.size := by
+      generalize hk : arr'.size - 1 - i - j = k
+      induction k generalizing arr' i j <;> unfold bubbleSort.loop₁.loop₂
+      case zero =>
+        split
+        case isTrue hlt => sorry
+        case isFalse hnlt => sorry
+      case succ n ih => sorry
+    rw[loop₂_size_eq]
+    rename_i h
+    exact Nat.sub_succ_lt_self arr.size i h
+  loop₁ arr 0
 
 theorem lt_of_lt_sub {a b c : Nat} : a < b - c → a < b := by
   intro h
@@ -12,6 +39,8 @@ theorem lt_of_lt_sub {a b c : Nat} : a < b - c → a < b := by
       a < b - c := h
       b - c ≤ b := Nat.sub_le b c
 
+namespace NotZenn
+
 def bubbleSort_for [Inhabited α] [Ord α] (arr : Array α) : Array α := Id.run do
   let mut arr := arr
   for i in [0:arr.size] do
@@ -23,26 +52,26 @@ def bubbleSort_for [Inhabited α] [Ord α] (arr : Array α) : Array α := Id.run
 
 def bubbleSort [Ord α] (arr : Array α) : Array α :=
   let rec loop₁ [Ord α] (arr : Array α) (i : Nat) : Array α :=
-    let rec loop₂ [Ord α] (arr : Array α) (i j : Fin arr.size) :=
+    let rec loop₂ [Ord α] (arr : Array α) (i j : Nat) :=
       if h₁ : j < arr.size - 1 - i then
-        haveI j_lt_arr_size_pred : j < arr.size - 1 := lt_of_lt_sub h₁
-        haveI succ_j_lt_arr_size : j + 1 < arr.size := Nat.add_lt_of_lt_sub j_lt_arr_size_pred
-        match Ord.compare arr[j] arr[↑j + 1] with
+        -- haveI j_lt_arr_size_pred : j < arr.size - 1 := lt_of_lt_sub h₁
+        -- haveI succ_j_lt_arr_size : j + 1 < arr.size := Nat.add_lt_of_lt_sub j_lt_arr_size_pred
+        match Ord.compare arr[j] arr[j + 1] with
         |.gt =>
-          haveI swap_size_eq : arr.size = (arr.swap j (j + 1)).size := Eq.symm (Array.size_swap arr j (j + 1))
-          haveI succ_j_lt_swapped : ↑j + 1 < (arr.swap j (j + 1)).size := Nat.lt_of_lt_of_eq succ_j_lt_arr_size swap_size_eq
-          loop₂ (arr.swap j (j + 1)) (i.cast swap_size_eq) ⟨↑j + 1, succ_j_lt_swapped⟩
-        |.lt |.eq => loop₂ arr i (⟨j + 1, succ_j_lt_arr_size⟩)
+          -- haveI swap_size_eq : arr.size = (arr.swap j (j + 1)).size := Eq.symm (Array.size_swap arr j (j + 1))
+          -- haveI succ_j_lt_swapped : j + 1 < (arr.swap j (j + 1)).size := Nat.lt_of_lt_of_eq succ_j_lt_arr_size swap_size_eq
+          loop₂ (arr.swap j (j + 1)) i (j + 1)
+        |.lt |.eq => loop₂ arr i (j + 1)
       else
         arr
     termination_by arr.size - 1 - i - j
     if h : i < arr.size then
-      loop₁ (loop₂ arr ⟨i, h⟩ ⟨0, Nat.zero_lt_of_lt h⟩) (i + 1)
+      loop₁ (loop₂ arr i 0) (i + 1)
     else
       arr
   termination_by arr.size - i
   decreasing_by
-    have loop₂_size_eq [Ord α] (arr' arr : Array α) {i j : Nat} {hi : i < arr'.size} {hj : j < arr'.size} (h_size : arr'.size = arr.size) : (bubbleSort.loop₁.loop₂ arr' ⟨i, hi⟩ ⟨j, hj⟩).size = arr.size := by
+    have loop₂_size_eq {arr' arr : Array α} {i j : Nat} (h_size : arr'.size = arr.size) : (bubbleSort.loop₁.loop₂ arr' i j).size = arr.size := by
       generalize h : arr'.size - 1 - i - j = k
       induction k generalizing arr' i j
       case zero =>
@@ -78,9 +107,11 @@ def bubbleSort [Ord α] (arr : Array α) : Array α :=
             exact h_eq_n
         case isFalse =>
           exact h_size
-    rw[loop₂_size_eq arr arr rfl]
+    rw[loop₂_size_eq rfl]
     exact Nat.sub_succ_lt_self arr.size i h
   loop₁ arr 0
 
 #eval bubbleSort_for #[100,90,45,32,56,44]
 #eval bubbleSort #[100,90,45,32,56,44]
+
+end NotZenn
